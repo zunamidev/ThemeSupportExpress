c08ae016d23dfcb9154045b19903faa4ef3497e1
Zunami <hamza@isotoxin.com>
2020-08-31 19:08:39 +0200

    🎨 Improvent: CLI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 'use strict';
+const readLine = require('readline');
+
+const args = require('minimist')(process.argv.slice(2), {
+  string: ['theme']
+});
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Who is the author ? ', function(name) {
+  console.log(`The author is ${name`);
+  rl.close();
+});
+
+rl.on('close', function() {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
